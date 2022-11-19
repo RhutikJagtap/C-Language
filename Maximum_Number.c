14c656ae29420d291cb43a5f4f51de4172cc98ab
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-19 18:30:11 +0530

    Add files via upload

---
+++ Maximum_Number.c
@@ -0,0 +1,17 @@
+//c programm to print maximum number
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter any two number:");
+    scanf("%d\n%d",&a,&b);
+    if(a>b)
+    {
+        printf("a is maximum");
+    }
+    else
+    {
+    printf("b is maximum");
+}
+}
+// Code By Rj
