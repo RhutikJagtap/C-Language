9e519cde8471e87168e6b6c28aaea51518fa84a4
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-19 18:34:39 +0530

    Add files via upload

---
+++ Odd_Even.c
@@ -0,0 +1,17 @@
+//c program to print odd or even number
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any number:");
+    scanf("%d",&n);
+if(n%2==0)
+{
+    printf("%d is even number",n);
+}
+else
+{
+printf("%d is odd number",n);
+}
+}
+// @Code By Rj
