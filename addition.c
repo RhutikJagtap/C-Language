27f2161731b274131ba81f3502888f8afdbe19ae
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-19 18:18:48 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+// c programme of addition of two numbers
+#include <stdio.h>
+void main()
+{
+  int a,b,sum;
+  printf("Enter two values:\n");
+  scanf("%d%d", &a,&b);
+  sum = a + b;
+  printf("addition of numbers= %d\n", sum);
+}
+// @Code By Rj
