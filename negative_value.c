0050f16bb172e450ac205a88fdf9b7fcac2171ff
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-19 18:48:43 +0530

    Add files via upload

---
+++ negative_value.c
@@ -0,0 +1,13 @@
+//c programme to print negative value
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any value:");
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("a is negative");
+    }
+}
+// @Code By Rj
